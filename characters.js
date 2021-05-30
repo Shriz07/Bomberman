8519bae77a95074362db52a1545c17cab64a275e
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-05-30 13:19:45 +0200

    Added characters

---
+++ characters.js
@@ -0,0 +1,40 @@
+const characters = [
+    {
+        "class_id": "1",
+        "class_name": "Zwinne nogi",
+        "speed": 2,
+        "bomb_amount": 1,
+        "bomb_range": 1,
+        "live": 3,
+        "description": "Zwiększa szybkość postaci o 1",
+      },
+      {
+        "class_id": "2",
+        "class_name": "Bomber",
+        "speed": 1,
+        "bomb_amount": 2,
+        "bomb_range": 1,
+        "live": 3,
+        "description": "Zwiększa liczbę bomb o 1",
+      },
+      {
+        "class_id": "3",
+        "class_name": "Medyk",
+        "speed": 1,
+        "bomb_amount": 1,
+        "bomb_range": 1,
+        "live": 4,
+        "description": "Zwiększa liczbę żyć o 1",
+      },
+      {
+        "class_id": "4",
+        "class_name": "Rage Bomber",
+        "speed": 1,
+        "bomb_amount": 1,
+        "bomb_range": 2,
+        "live": 3,
+        "description": "Zwiększa zasięg bomb o 1",
+      }
+]
+
+module.exports = {characters}
