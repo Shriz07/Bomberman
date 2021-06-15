36230006a97be0b3c223c61b8ae79eceb786e2e4
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-15 23:39:30 +0200

    Styling and formatting change

--- public/javascripts/content_handler.js
+++ public/javascripts/content_handler.js
@@ -5,140 +5,115 @@ let canMove = true;
 let allUsers = null;
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-window.addEventListener('keydown', e => {
-    if(e.key === 'ArrowUp' && canMove)
-    {
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp" && canMove) {
         let inputDirection = "up";
-        socket.emit('request move', {direction: inputDirection});
+        socket.emit("request move", { direction: inputDirection });
         canMove = false;
-    }
-    else if(e.key === 'ArrowDown' && canMove)
-    {
+    } else if (e.key === "ArrowDown" && canMove) {
         let inputDirection = "back";
-        socket.emit('request move', {direction: inputDirection});
+        socket.emit("request move", { direction: inputDirection });
         canMove = false;
-    }
-    else if(e.key === 'ArrowLeft' && canMove)
-    {
+    } else if (e.key === "ArrowLeft" && canMove) {
         let inputDirection = "left";
-        socket.emit('request move', {direction: inputDirection});
+        socket.emit("request move", { direction: inputDirection });
         canMove = false;
-    }
-    else if(e.key === 'ArrowRight' && canMove)
-    {
+    } else if (e.key === "ArrowRight" && canMove) {
         let inputDirection = "right";
-        socket.emit('request move', {direction: inputDirection});
+        socket.emit("request move", { direction: inputDirection });
         canMove = false;
-    }
-    else if(e.keyCode === 32)
-        socket.emit('request place bomb', {});
-})
-
+    } else if (e.keyCode === 32) socket.emit("request place bomb", {});
+});
 
 let lastRenderTime = 0;
 let characterSpeed = 1;
 
-function main(currentTime)
-{
+function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if(secondsSinceLastRender < 1 / characterSpeed)
-        return;
+    if (secondsSinceLastRender < 1 / characterSpeed) return;
     canMove = true;
     lastRenderTime = currentTime;
 }
 
-function chooseCharacter()
-{
-    var character = document.getElementById('characterSelect');
-    socket.emit('login', {UID: UID, class_id: character.value})
+function chooseCharacter() {
+    var character = document.getElementById("characterSelect");
+    socket.emit("login", { UID: UID, class_id: character.value });
     window.requestAnimationFrame(main);
 }
 
-function drawMap()
-{
-    const gameBoard = document.getElementById('game-board');
+function drawMap() {
+    const gameBoard = document.getElementById("game-board");
 
-    for(let i = 0; i < map[0].length; i++)
-    {
-        for(let j = 0; j < map.length; j++)
-        {
-            if(map[j][i] === 0)
-                placeEmptySpace(i, j);
-            if(map[j][i] === 1)
-                placeWall(i, j);
-            if(map[j][i] === 2)
-            placeDestructibleWall(i, j);
+    for (let i = 0; i < map[0].length; i++) {
+        for (let j = 0; j < map.length; j++) {
+            if (map[j][i] === 0) placeEmptySpace(i, j);
+            if (map[j][i] === 1) placeWall(i, j);
+            if (map[j][i] === 2) placeDestructibleWall(i, j);
         }
     }
 }
 
-function placeEmptySpace(x, y)
-{
-    const gameBoard = document.getElementById('game-board');
+function placeEmptySpace(x, y) {
+    const gameBoard = document.getElementById("game-board");
 
-    const emptySpace = document.createElement('div');
-    emptySpace.style.gridColumnStart = x+1;
-    emptySpace.style.gridRowStart = y+1;
-    emptySpace.classList.add('emptySpace');
+    const emptySpace = document.createElement("div");
+    emptySpace.style.gridColumnStart = x + 1;
+    emptySpace.style.gridRowStart = y + 1;
+    emptySpace.classList.add("emptySpace");
     gameBoard.appendChild(emptySpace);
 }
 
-function placeWall(x, y)
-{
-    const gameBoard = document.getElementById('game-board');
+function placeWall(x, y) {
+    const gameBoard = document.getElementById("game-board");
 
-    const wall = document.createElement('div');
-    wall.style.gridColumnStart = x+1;
-    wall.style.gridRowStart = y+1;
-    wall.classList.add('wall');
+    const wall = document.createElement("div");
+    wall.style.gridColumnStart = x + 1;
+    wall.style.gridRowStart = y + 1;
+    wall.classList.add("wall");
     gameBoard.appendChild(wall);
 }
 
-function placeDestructibleWall(x, y)
-{
-    const gameBoard = document.getElementById('game-board');
+function placeDestructibleWall(x, y) {
+    const gameBoard = document.getElementById("game-board");
 
-    const wall = document.createElement('div');
-    wall.style.gridColumnStart = x+1;
-    wall.style.gridRowStart = y+1;
-    wall.classList.add('destructibleWall');
+    const wall = document.createElement("div");
+    wall.style.gridColumnStart = x + 1;
+    wall.style.gridRowStart = y + 1;
+    wall.classList.add("destructibleWall");
     gameBoard.appendChild(wall);
 }
 
-function placeBomb(x, y)
-{
-    const gameBoard = document.getElementById('game-board');
+function placeBomb(x, y) {
+    const gameBoard = document.getElementById("game-board");
 
-    const wall = document.createElement('div');
-    wall.style.gridColumnStart = x+1;
-    wall.style.gridRowStart = y+1;
-    wall.classList.add('bomb');
+    const wall = document.createElement("div");
+    wall.style.gridColumnStart = x + 1;
+    wall.style.gridRowStart = y + 1;
+    wall.classList.add("bomb");
     gameBoard.appendChild(wall);
 }
 
-function placePlayer(playerID, color, x, y)
-{
-    const gameBoard = document.getElementById('game-board');
+function placePlayer(playerID, color, x, y) {
+    const gameBoard = document.getElementById("game-board");
 
-    const player = document.createElement('div');
+    const player = document.createElement("div");
     player.style = `background-image: url('../images/${color}.png'); z-index: 100; background-size: contain; background-repeat: no-repeat; background-position: center;`;
-    player.style.gridColumnStart = x+1;
-    player.style.gridRowStart = y+1;
+    player.style.gridColumnStart = x + 1;
+    player.style.gridRowStart = y + 1;
     player.classList.add(playerID);
     gameBoard.appendChild(player);
 }
 
-async function placeExplosion(x, y)
-{
-    const gameBoard = document.getElementById('game-board');
-    const explode = document.createElement('div');
-    explode.style.gridColumnStart = x+1;
-    explode.style.gridRowStart = y+1;
-    explode.classList.add('explode');
+async function placeExplosion(x, y) {
+    const gameBoard = document.getElementById("game-board");
+    const explode = document.createElement("div");
+    explode.style.gridColumnStart = x + 1;
+    explode.style.gridRowStart = y + 1;
+    explode.classList.add("explode");
     gameBoard.appendChild(explode);
 
     await sleep(1000);
@@ -146,85 +121,83 @@ async function placeExplosion(x, y)
     gameBoard.removeChild(explode);
 }
 
-async function makePlayerImmortal(playerID, time)
-{
-    const gameBoard = document.getElementById('game-board');
+async function makePlayerImmortal(playerID, time) {
+    const gameBoard = document.getElementById("game-board");
     const player = document.getElementsByClassName(playerID);
-    while(time > 0)
-    {
-        player[0].style.opacity = '0.5';
+    while (time > 0) {
+        player[0].style.opacity = "0.5";
         await sleep(250);
-        player[0].style.opacity = '1';
+        player[0].style.opacity = "1";
         await sleep(250);
         time -= 500;
     }
 }
 
-async function bombExplode(x, y, radius)
-{
+async function bombExplode(x, y, radius) {
     placeExplosion(x, y);
 
-    let flagLeft = flagRight = flagUp = flagDown = true;
-    if(map[y-1][x] === 1) //down
+    let flagLeft = (flagRight = flagUp = flagDown = true);
+    if (map[y - 1][x] === 1)
+        //down
         flagDown = false;
-    if(map[y+1][x] === 1) //up
+    if (map[y + 1][x] === 1)
+        //up
         flagUp = false;
-    if(map[y][x-1] === 1) //left
+    if (map[y][x - 1] === 1)
+        //left
         flagLeft = false;
-    if(map[y][x+1] === 1) //right
-        flagRight = false; 
+    if (map[y][x + 1] === 1)
+        //right
+        flagRight = false;
 
-    for(let i = 1; i <= radius; i++)
-    {
-        if(x - i > 0 && flagLeft)
-            placeExplosion(x-i, y);
-        if(x + i < map[0].length && flagRight)
-            placeExplosion(x+i, y);
-        if(y - i > 0 && flagUp)
-            placeExplosion(x, y-i);
-        if(y + i < map.length && flagDown)
-            placeExplosion(x, y+i);
+    for (let i = 1; i <= radius; i++) {
+        if (x - i > 0 && flagLeft) placeExplosion(x - i, y);
+        if (x + i < map[0].length && flagRight) placeExplosion(x + i, y);
+        if (y - i > 0 && flagUp) placeExplosion(x, y - i);
+        if (y + i < map.length && flagDown) placeExplosion(x, y + i);
     }
 }
 
-function refreshUserStatistics()
-{
-    document.getElementById('statistics').innerHTML = '';
-    allUsers.forEach(u => {
-        const stats = document.createElement('div');
-        stats.style = 'border: 5px solid black;';
+function refreshUserStatistics() {
+    document.getElementById("statistics").innerHTML = "";
+    allUsers.forEach((u) => {
+        const stats = document.createElement("div");
+        stats.id = "playerStat";
         const playerName = document.createElement("H2");
         const className = document.createElement("H3");
         const lives = document.createElement("H3");
         const points = document.createElement("H3");
-        playerName.appendChild(document.createTextNode('Player name: ' + u.UID));
-        className.appendChild(document.createTextNode('Player class: ' + u.className));
-        lives.appendChild(document.createTextNode('Lives: ' + u.lives));
-        points.appendChild(document.createTextNode('Points: ' + u.points));
+        playerName.appendChild(
+            document.createTextNode("Player name: " + u.UID)
+        );
+        className.appendChild(
+            document.createTextNode("Player class: " + u.className)
+        );
+        lives.appendChild(document.createTextNode("Lives: " + u.lives));
+        points.appendChild(document.createTextNode("Points: " + u.points));
 
         stats.appendChild(playerName);
         stats.appendChild(className);
         stats.appendChild(lives);
         stats.appendChild(points);
-        
-        document.getElementById('statistics').appendChild(stats);
+
+        document.getElementById("statistics").appendChild(stats);
     });
 }
 
-function decreasePlayerLives(UID)
-{
-    allUsers.forEach(u => {
-        if(u.UID === UID)
-            u.lives--;
+function decreasePlayerLives(UID) {
+    allUsers.forEach((u) => {
+        if (u.UID === UID) u.lives--;
     });
 }
 
-socket.on('loggedIn', function(data) {
+socket.on("loggedIn", function (data) {
     user = data.user;
     characterSpeed = user.speed;
-    console.log('Login success');
-    document.getElementsByClassName('container')[0].style.display = 'inherit';
-    document.getElementsByTagName('body')[0].style = 'padding: 0px; height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center; margin: 0; background-color: black;';
+    console.log("Login success");
+    document.getElementsByClassName("container")[0].style.display = "inherit";
+    document.getElementsByTagName("body")[0].style =
+        "padding: 0px; height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center; margin: 0; background-color: black;";
     $("#container").empty();
     $("#container").append("<div id='game-board'></div>");
     $("#container").append("<div id='statistics'></div>");
@@ -232,56 +205,55 @@ socket.on('loggedIn', function(data) {
     map = data.map;
     drawMap();
 
-    placePlayer(data.user.UID, data.user.color, data.user.player_xy.x, data.user.player_xy.y);
+    placePlayer(
+        data.user.UID,
+        data.user.color,
+        data.user.player_xy.x,
+        data.user.player_xy.y
+    );
 });
 
-
-socket.on('update player statistics', function(data) {
+socket.on("update player statistics", function (data) {
     allUsers = data.users;
-    data.users.forEach(u => {
-        if(u.UID !== user.UID)
-        {
+    data.users.forEach((u) => {
+        if (u.UID !== user.UID) {
             placePlayer(u.UID, u.color, u.player_xy.x, u.player_xy.y);
         }
     });
     refreshUserStatistics();
 });
 
-socket.on('move player', function(data) {
-    const gameBoard = document.getElementById('game-board');
+socket.on("move player", function (data) {
+    const gameBoard = document.getElementById("game-board");
     const player = document.getElementsByClassName(data.UID);
-    player[0].style.gridColumnStart = data.player_xy.x+1;
-    player[0].style.gridRowStart = data.player_xy.y+1;
+    player[0].style.gridColumnStart = data.player_xy.x + 1;
+    player[0].style.gridRowStart = data.player_xy.y + 1;
 });
 
-socket.on('place bomb', function(data) {
+socket.on("place bomb", function (data) {
     placeBomb(data.bomb_xy.x, data.bomb_xy.y);
 });
 
-socket.on('place explode', function(data) {
+socket.on("place explode", function (data) {
     placeEmptySpace(data.bomb_xy.x, data.bomb_xy.y);
     bombExplode(data.bomb_xy.x, data.bomb_xy.y, data.radius);
 });
 
-socket.on('remove block', function(data) {
-    data.blocks.forEach(block => {
+socket.on("remove block", function (data) {
+    data.blocks.forEach((block) => {
         placeEmptySpace(block.x, block.y);
     });
 });
 
-socket.on('hit player', function(data) {
-    if(data.status === 'dead')
-    {
-        if(user.UID === data.UID)
-            socket.emit('end', {});
-        $('.' + data.UID).remove();
-    }
-    else
-        makePlayerImmortal(data.UID, data.immortal_time);
+socket.on("hit player", function (data) {
+    if (data.status === "dead") {
+        if (user.UID === data.UID) socket.emit("end", {});
+        $("." + data.UID).remove();
+    } else makePlayerImmortal(data.UID, data.immortal_time);
     decreasePlayerLives(data.UID);
     refreshUserStatistics();
 });
 
-socket.on('game over', function(data) {
+socket.on("game over", function (data) {
     alert(`Player ${data.winner} has won`);
-})
+});
