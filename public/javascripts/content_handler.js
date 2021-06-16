1f19eadace4280e3236bdc2f8d1aa9401b6b837d
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-16 10:50:00 +0200

    Added users avatars and fixed the font sizing

--- public/javascripts/content_handler.js
+++ public/javascripts/content_handler.js
@@ -163,6 +163,9 @@ function refreshUserStatistics() {
     allUsers.forEach((u) => {
         const stats = document.createElement("div");
         stats.id = "playerStat";
+        const playerAvatar = document.createElement("img");
+        playerAvatar.src = `../images/${u.color}.png`;
+        playerAvatar.id = `playerAvatar`;
         const playerName = document.createElement("H2");
         const className = document.createElement("H3");
         const lives = document.createElement("H3");
@@ -180,7 +183,7 @@ function refreshUserStatistics() {
         stats.appendChild(className);
         stats.appendChild(lives);
         stats.appendChild(points);
-
+        stats.appendChild(playerAvatar);
         document.getElementById("statistics").appendChild(stats);
     });
 }
